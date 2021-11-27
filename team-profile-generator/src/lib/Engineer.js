4675592c1df3d2bea2e838c8b91a2bd0a63b521f
anabwarsame <anab_1994@hotmail.co.uk>
2021-11-27 21:27:05 +0000

    created engineer

--- team-profile-generator/src/lib/Engineer.js
+++ team-profile-generator/src/lib/Engineer.js
@@ -1,6 +1,17 @@
+const Employee = require("./Employee.js");
 class Engineer extends Employee {
-  constructor({ name, id, email, github }) {}
+  constructor({ name, id, email, github }) {
+    super(name, id, email);
+    this.github = github;
+  }
 
-  getGithub() {}
-  getRole() {}
+  getGithub() {
+    return this.github;
+  }
+  getRole() {
+    return "Engineer";
+  }
 }
+
+let engineer = new Engineer("anab", 1234, "anab@gmail.com", "dinosaur");
+console.log(engineer.getRole());
