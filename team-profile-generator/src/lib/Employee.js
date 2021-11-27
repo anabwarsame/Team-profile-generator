edfb74f38b7c1192b9899785dd52ad1d4f92a6c0
anabwarsame <anab_1994@hotmail.co.uk>
2021-11-27 21:15:08 +0000

    created employee

--- team-profile-generator/src/lib/Employee.js
+++ team-profile-generator/src/lib/Employee.js
@@ -1,7 +1,20 @@
 class Employee {
-  constructor({ name, id, email }) {}
-  getName() {}
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {
+    return this.name;
+  }
   getId() {}
   getEmail() {}
-  getRole() {}
+  getRole() {
+    return "Employee";
+  }
 }
+
+let employee = new Employee("anab", 1234, "anab@gmail.com");
+console.log(employee.getRole());
+
+module.exports = Employee;
