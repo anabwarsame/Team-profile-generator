1d7f378399d0fce589190f4ae88af45ebcdd51d0
anabwarsame <anab_1994@hotmail.co.uk>
2021-11-27 22:31:18 +0000

    prompting questions wip

--- team-profile-generator/src/index.js
+++ team-profile-generator/src/index.js
@@ -1,11 +1,36 @@
 const inquirer = require("inquirer");
+
 inquirer
   .prompt([
+    {
+      type: "input",
+      message: "what is the name of your team?",
+    },
     {
       type: "list",
       message: "pick the type of employee:",
       name: "role",
       choices: ["manager", "engineer", "intern"],
+    },
+    {
+      type: "input",
+      name: "managers name",
+      message: "enter manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "enter manager employee id",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "enter manager email address",
+    },
+    {
+      type: "input",
+      name: "office number",
+      message: "enter manager office number",
     },
   ])
   .then((answers) => {
