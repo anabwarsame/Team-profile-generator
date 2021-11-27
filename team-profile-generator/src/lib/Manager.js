03138fa75477a6426cac2104fefe67509f10062c
anabwarsame <anab_1994@hotmail.co.uk>
2021-11-27 21:31:16 +0000

    created manager

--- team-profile-generator/src/lib/Manager.js
+++ team-profile-generator/src/lib/Manager.js
@@ -1,6 +1,17 @@
+const Employee = require("./Employee.js");
 class Manager extends Employee {
-  constructor({ name, id, email, officeNumber }) {}
+  constructor({ name, id, email, officeNumber }) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
 
-  getOfficeNumber() {}
-  getRole() {}
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+  getRole() {
+    return "manager";
+  }
 }
+
+let manager = new Manager("anab", 1234, "anab@gmail.com", "12345");
+console.log(manager.getOfficeNumber());
